90298ac45c7815b013d987a9cafa3a7d58cb1a1e
anulisba <anulisba@gmail.com>
2024-08-31 19:39:01 +0530

    trau

--- src/components/ActivityInfo.js
+++ src/components/ActivityInfo.js
@@ -145,9 +145,9 @@ const ActivityInfo = () => {
 
                     <h3 className="activity-info-trainers-heading">Trained by</h3>
                     <div className="activity-info-trainers">
-                        <img src={Trainer1} alt="trainer-image" className="activity-info-trainer-image" />
-                        <img src={Trainer2} alt="trainer-image" className="activity-info-trainer-image" />
-                        <img src={Trainer1} alt="trainer-image" className="activity-info-trainer-image" />
+                        <img src={ActivityImg1} alt="trainer" className="activity-info-trainer-image" />
+                        <img src={Trainer2} alt="trainer" className="activity-info-trainer-image" />
+                        <img src={Trainer1} alt="trainer" className="activity-info-trainer-image" />
                         <img src={Trainer2} alt="trainer-image" className="activity-info-trainer-image" />
                     </div>
                 </div>
