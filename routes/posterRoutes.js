b3548ec99b27dfa2c819d93dd0a5cbc17d5d08ce
sweetysara <sweetysarasunny@gmail.com>
2024-09-21 11:50:38 +0530

    provider fetch issue

--- routes/posterRoutes.js
+++ routes/posterRoutes.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const multer = require('multer');
 const Poster = require('../models/Poster'); // Adjust the path as necessary
 
 const router = express.Router();
 
-// Set up multer for file handling
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+
 
 // Route to fetch all posters or wishlist posters based on query parameter
 router.get('/', async (req, res) => {
@@ -43,58 +40,5 @@ router.get('/:id', async (req, res) => {
     res.status(500).json({ message: 'Internal server error. Please try again later.' });
   }
 });
-
-// Route to update a specific poster by ID
-router.put('/:id', upload.single('image'), async (req, res) => {
-  const { id } = req.params;
-  const { name, description, location, phone, startDate, endDate } = req.body;
-  let imageBase64 = null;
-
-  if (req.file) {
-    imageBase64 = req.file.buffer.toString('base64');
-  }
-
-  try {
-    const updatedPoster = await Poster.findByIdAndUpdate(id, {
-      name,
-      description,
-      location,
-      phone,
-      startDate,
-      endDate,
-      image: imageBase64 || undefined, // Only update image if a new file is provided
-    }, { new: true });
-
-    if (!updatedPoster) {
-      return res.status(404).json({ message: 'Poster not found' });
-    }
-
-    res.status(200).json(updatedPoster);
-  } catch (error) {
-    console.error('Error updating poster:', error);
-    res.status(500).json({ message: 'Internal server error. Please try again later.' });
-  }
-});
-
-
-// Route to update the wishlist status of a poster
-router.put('/:id/wishlist', async (req, res) => {
-  const { id } = req.params;
-  const { wishlist } = req.body;
-
-  try {
-    const updatedPoster = await Poster.findByIdAndUpdate(id, { wishlist }, { new: true });
-
-    if (!updatedPoster) {
-      return res.status(404).json({ message: 'Poster not found' });
-    }
-
-    res.status(200).json(updatedPoster);
-  } catch (error) {
-    console.error('Error updating wishlist:', error);
-    res.status(500).json({ message: 'Internal server error. Please try again later.' });
-  }
-});
-
 
 module.exports = router;
